61ec6bacc3240c4a556dfb3cd9489117b4a13b72
ryan-mcomber <ryanmcom@hotmail.com>
2021-10-20 12:02:07 -0600

    classes to interfaces for Dao

--- src/main/java/com/revature/repository/CurriculumDao.java
+++ src/main/java/com/revature/repository/CurriculumDao.java
@@ -1,5 +1,9 @@
 package com.revature.repository;
 
-public class CurriculumDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.revature.model.Curriculum;
+
+public interface CurriculumDao  extends JpaRepository<Curriculum, Integer>{
 
 }
