812e6f2573fc875610380e3866b14b1a91ee8d60
ryan-mcomber <35877844+ryan-mcomber@users.noreply.github.com>
2021-10-21 15:02:14 -0600

    Update TechnologyDao.java

--- src/main/java/com/revature/repository/TechnologyDao.java
+++ src/main/java/com/revature/repository/TechnologyDao.java
@@ -1,7 +1,5 @@
 package com.revature.repository;
 
-import java.util.Optional;
-import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.revature.model.Technology;
