1ff90ecd72a7b026ca9130120b769a72e98b7833
ImCoolYeah105 <maw23456789@gmail.com>
2021-10-21 12:57:08 -0400

    update topic repository

--- src/main/java/com/revature/repository/TopicDao.java
+++ src/main/java/com/revature/repository/TopicDao.java
@@ -1,11 +1,17 @@
 package com.revature.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.model.Topic;
 
 @Repository
-public interface TopicDao  extends JpaRepository<Topic, Integer>{
+public interface TopicDao extends JpaRepository<Topic, Integer> {
+	
+	List<TopicDao> findByDate(int day);
+	List<TopicDao> findByName(String name);
+	List<TopicDao> findByTechnology(int id);
 
 }
