9c83c0e04af3398e049ed020697385a73a427f5f
ryan-mcomber <35877844+ryan-mcomber@users.noreply.github.com>
2021-10-22 12:13:18 -0600

    Update Topic.java

--- src/main/java/com/revature/model/Topic.java
+++ src/main/java/com/revature/model/Topic.java
@@ -15,7 +15,7 @@ import javax.persistence.Column;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+
 import javax.persistence.FetchType;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
