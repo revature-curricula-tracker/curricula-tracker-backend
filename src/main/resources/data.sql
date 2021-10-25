2b267dfebee10a6962c309b873421282c3934c3e
jmliguid <joshualiguid@gmail.com>
2021-10-25 09:04:28 -0700

    Added dao, service and controller methods for CurriculumTopic model.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,5 @@ INSERT INTO curriculum (curriculum_name,num_days,num_weeks) VALUES ('Java: Enter
 INSERT INTO technology (tech_name) VALUES ('Java');
 INSERT INTO topic (name,technology_tech_id) VALUES ('Basic Java',1);
 INSERT INTO curriculum_topic(curriculum_id,topic_id,topic_day) VALUES (1,1,5);
+INSERT INTO topic (name,technology_tech_id) VALUES ('Advanced Java',1);
+
