5cb6cdad1692a686b64ec82c598d1e86649c0554
Noah Gaston <nrgman@tds.net>
2021-10-25 10:10:38 -0500

    trimming

--- src/main/java/com/revature/exceptions/TechnologyNotFoundException.java
+++ src/main/java/com/revature/exceptions/TechnologyNotFoundException.java
@@ -5,16 +5,4 @@ public class TechnologyNotFoundException extends RuntimeException{
 	public TechnologyNotFoundException(String message) {
 		super(message);
 	}
-
-	public TechnologyNotFoundException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-	}
-
-	public TechnologyNotFoundException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
-	}
-
-	public TechnologyNotFoundException(Throwable arg0) {
-		super(arg0);
-	}
 }
