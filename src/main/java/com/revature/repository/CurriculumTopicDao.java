d4ea0af28fd17404bb36d8c927eebd6b0d4ec98a
ryan-mcomber <ryanmcom@hotmail.com>
2021-10-26 13:04:26 -0600

    progress on join table

--- src/main/java/com/revature/repository/CurriculumTopicDao.java
+++ src/main/java/com/revature/repository/CurriculumTopicDao.java
@@ -1,8 +1,11 @@
 package com.revature.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.model.CurriculumTopic;
 import com.revature.model.CurriculumTopicKey;
@@ -12,7 +15,9 @@ public interface CurriculumTopicDao extends JpaRepository<CurriculumTopic, Integ
 
 	CurriculumTopic findByCurriculumTopicKey(CurriculumTopicKey id);
 	
-//	@Query("Delete from ")
-//    void deleteByCuriculumId(int CurriculumId);
+	 @Transactional
+	 @Modifying
+     @Query("DELETE FROM CurriculumTopic WHERE curriculum_id =:#{#id}") // 
+     void deleteByCuriculumId(@Param("id")int CurriculumId);
 	
 }
