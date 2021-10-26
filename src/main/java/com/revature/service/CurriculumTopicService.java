3c76e7e63f8613aaaec8087e46b75c611306f03b
tdj182 <tdj182@gmail.com>
2021-10-26 10:46:26 -0500

    added some crud methods to CT

--- src/main/java/com/revature/service/CurriculumTopicService.java
+++ src/main/java/com/revature/service/CurriculumTopicService.java
@@ -1,8 +1,33 @@
 package com.revature.service;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.model.CurriculumTopic;
+import com.revature.model.CurriculumTopicKey;
+import com.revature.repository.CurriculumTopicDao;
 
 @Service
 public class CurriculumTopicService {	
+
+	@Autowired
+	private CurriculumTopicDao ctDAO;
+
+	public CurriculumTopic insert(@Valid CurriculumTopic ct) {
+		return ctDAO.save(ct);
+	}
 	
+	@Transactional(readOnly = true)
+	public List<CurriculumTopic> findAll() {
+		return ctDAO.findAll();
+	}
+	
+//	public void removeByIds(int curriculumId, int topicId) {
+//		ctDAO.CurriculumTopicDelete(new CurriculumTopicKey(curriculumId, topicId));
+//	}
 }
