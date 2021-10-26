d6c3cff0f2d3611cff3be7604f9623c84d23f30c
Jasper Sommer <jasper.sommer97@gmail.com>
2021-10-26 15:05:43 -0400

    controller tests for technology

--- src/main/java/com/revature/controller/TechnologyController.java
+++ src/main/java/com/revature/controller/TechnologyController.java
@@ -3,6 +3,8 @@ package com.revature.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.Technology;
@@ -27,12 +28,12 @@ public class TechnologyController {
 	public TechnologyService techServ;
 
 	@GetMapping("/{id}")
-	public Optional<Technology> findByTechId(@RequestParam("id") int id) {
+	public Optional<Technology> findByTechId(@PathVariable ("id") int id) {
 		return techServ.getById(id);
 	}
 
 	@GetMapping("/search/{name}")
-	public Optional<Technology> findByTechName(@RequestParam("Name") String name) {
+	public Optional<Technology> findByTechName(@PathVariable("name") String name) {
 		return techServ.getByName(name);
 	}
 
@@ -42,7 +43,7 @@ public class TechnologyController {
 	}
 
 	@PostMapping("/add")
-	public Technology insert(@RequestBody Technology t) {
+	public Technology insert(@Valid @RequestBody Technology t) {
 		return techServ.insertTechnology(t);
 	}
 
