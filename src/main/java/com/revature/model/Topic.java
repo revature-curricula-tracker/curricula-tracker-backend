0c0e6e3fca18f17022f67761f0b633072c7908e3
ryan-mcomber <35877844+ryan-mcomber@users.noreply.github.com>
2021-10-28 14:27:02 -0600

    Update Topic.java
    removed an import for CascadeType

--- src/main/java/com/revature/model/Topic.java
+++ src/main/java/com/revature/model/Topic.java
@@ -3,7 +3,7 @@ package com.revature.model;
 import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
