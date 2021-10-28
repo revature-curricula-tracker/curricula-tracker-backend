f2317c4e2dcb55ad1e8a33cb88af06a5f0846aab
Jasper Sommer <jasper.sommer97@gmail.com>
2021-10-28 10:56:58 -0400

    added CurriculumTopicService Tests

---
+++ src/test/java/com/revature/serviceTests/CurriculumTopicServiceTests.java
@@ -0,0 +1,81 @@
+package com.revature.serviceTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.revature.model.Curriculum;
+import com.revature.model.CurriculumTopic;
+import com.revature.model.CurriculumTopicKey;
+import com.revature.model.Topic;
+import com.revature.repository.CurriculumDao;
+import com.revature.repository.CurriculumTopicDao;
+import com.revature.service.CurriculumService;
+import com.revature.service.CurriculumTopicService;
+
+@SpringBootTest(classes= {CurriculumTopicService.class,CurriculumTopicDao.class})
+public class CurriculumTopicServiceTests {
+	
+	@MockBean
+	CurriculumTopicDao ctDao;
+
+	@InjectMocks
+	CurriculumTopicService ctserv;
+
+	CurriculumTopic ct;
+	CurriculumTopicKey id;
+	
+	
+	@BeforeEach
+	void construct() {
+		MockitoAnnotations.openMocks(this);
+
+		this.id = new CurriculumTopicKey();
+		id.setCurriculumId(1);
+		id.setTopicId(1);
+		this.ct = new CurriculumTopic(id, new Curriculum(), new Topic(), 1);
+	}
+	
+	@Test
+	void insertCurriculumTest() {
+		when(this.ctDao.save(ct)).thenReturn(this.ct);
+		assertEquals(ctserv.insert(ct), this.ct);
+	}
+
+	@Test
+	void findAll_Test() {
+		List<CurriculumTopic> cts = new ArrayList<CurriculumTopic>();
+		cts.add(ct);
+		
+		when(this.ctDao.findAll()).thenReturn(cts);
+		assertEquals(cts, ctserv.findAll());
+	}
+	
+	@Test
+	void findByIds_Test() {
+		when(this.ctDao.findByCurriculumTopicKey(id)).thenReturn(ct);
+		assertEquals(ct, ctDao.findByCurriculumTopicKey(id));
+	}
+	
+	@Test
+	void testRemoveByIds() {
+		when(this.ctDao.findByCurriculumTopicKey(id)).thenReturn(ct);
+		
+		assertEquals(true, ctserv.removeByIds(1, 1));
+	}
+	
+	@Test
+	void testRemoveByIds_Error() {
+		assertEquals(false, ctserv.removeByIds(1, 1));
+	}
+}
