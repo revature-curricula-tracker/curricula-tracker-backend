7432f9befb1d81f0220c1811d2567786c3a8ef99
ImCoolYeah105 <maw23456789@gmail.com>
2021-11-01 12:42:16 -0400

    add update and delete topic by name

--- src/main/java/com/revature/exceptions/TechnologyNotFoundException.java
+++ src/main/java/com/revature/exceptions/TechnologyNotFoundException.java
@@ -2,7 +2,10 @@ package com.revature.exceptions;
 import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TechnologyNotFoundException extends RuntimeException{
-	public TechnologyNotFoundException(String message) {
+
+	private static final long serialVersionUID = 5526290251533749665L;
+
+	public TechnologyNotFoundException(final String message) {
 		super(message);
 	}
 }
