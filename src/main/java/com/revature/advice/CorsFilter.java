5f805238f873f163c5eed42c8a87f217cdb32767
ryan-mcomber <35877844+ryan-mcomber@users.noreply.github.com>
2021-11-03 08:24:59 -0600

    Update CorsFilter.java

--- src/main/java/com/revature/advice/CorsFilter.java
+++ src/main/java/com/revature/advice/CorsFilter.java
@@ -15,8 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @WebFilter("/*")
 public class CorsFilter extends OncePerRequestFilter {
 	public static final String ORIGIN = "http://curricula-tracker.s3-website-us-east-1.amazonaws.com/";
-	public static final String LOCALHOST = "http://localhost:4200";
-	public static final String ORIGIN_NEW = "http://54.161.65.198:5000";
+	public static final String ORIGIN_NEW = "http://http://curricula-tracker.s3-website-us-east-1.amazonaws.com/";
 	
 	
 	
