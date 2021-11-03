28039a2ac97a6261879f0e41cc20fa9191e2c269
ryan-mcomber <ryanmcom@hotmail.com>
2021-11-03 10:16:01 -0600

    fixed imports

--- src/main/java/com/revature/controller/TopicController.java
+++ src/main/java/com/revature/controller/TopicController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.advice.CorsFilter;
 import com.revature.model.Topic;
 import com.revature.model.json.TopicJson;
 import com.revature.service.TopicService;
