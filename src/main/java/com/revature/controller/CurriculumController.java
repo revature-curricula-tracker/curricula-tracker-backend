a4bee07b73df0ffd8acfa18721ed874154032558
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-11-03 12:08:34 -0400

    add lcoalhost to corsfilter to be removed by ryans changes

--- src/main/java/com/revature/controller/CurriculumController.java
+++ src/main/java/com/revature/controller/CurriculumController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.advice.CorsFilter;
 import com.revature.model.Curriculum;
 import com.revature.service.CurriculumService;
 
 @RestController
 @RequestMapping("/curriculum")
+@CrossOrigin(origins = {CorsFilter.ORIGIN,CorsFilter.LOCALHOST,CorsFilter.ORIGIN_NEW})
 public class CurriculumController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CurriculumController.class);
